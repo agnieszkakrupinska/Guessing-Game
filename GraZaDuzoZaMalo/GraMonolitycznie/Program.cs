401b96257e9b54e77376822aa01f376912749ace
Agnieszka Krupińska <48626047+agnieszkakrupinska@users.noreply.github.com>
2019-03-27 22:05:04 +0100

    Updated Version 1

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,11 +10,30 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
+            //deklaracja imienia
             Console.WriteLine("Witaj");
             Console.Write("Podaj swoje imie: ");
             string imie = Console.ReadLine();
-            Console.WriteLine($"Witaj {imie}!");
+            //Console.WriteLine($"Witaj {imie}!");
 
+            //deklaracja nazwiska
+            Console.Write("Podaj nazwisko: ");
+            string nazwisko = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie} {nazwisko} :)");
+
+            //deklaracja i konwertowanie wieku
+            Console.WriteLine("Teraz podaj swoj wiek: ");
+            int wiek = Convert.ToInt32(Console.ReadLine());
+
+            if (wiek < 67)
+            {
+                Console.WriteLine("do emerytury zostalo ci {0} lat", 67 - wiek);
+            }
+            else
+            {
+                Console.WriteLine("Emeryt :)");
+            }
+            Console.ReadKey();
         }
     }
 }
