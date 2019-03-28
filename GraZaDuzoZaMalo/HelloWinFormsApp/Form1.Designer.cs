f852c9bba24492cdebe98a48518ba5ff100b74a9
Agnieszka Krupińska <48626047+agnieszkakrupinska@users.noreply.github.com>
2019-03-28 12:48:43 +0100

    Version 1.4
    Desktop App (WinForm)

---
+++ GraZaDuzoZaMalo/HelloWinFormsApp/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace HelloWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelImie = new System.Windows.Forms.Label();
+            this.labelNazwisko = new System.Windows.Forms.Label();
+            this.labelWiek = new System.Windows.Forms.Label();
+            this.textBoxImie = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxNazwisko = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxWiek = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelKomunikat1 = new System.Windows.Forms.Label();
+            this.labelKomunikat2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelImie
+            // 
+            this.labelImie.AutoSize = true;
+            this.labelImie.Location = new System.Drawing.Point(37, 31);
+            this.labelImie.Name = "labelImie";
+            this.labelImie.Size = new System.Drawing.Size(26, 13);
+            this.labelImie.TabIndex = 0;
+            this.labelImie.Text = "Imię";
+            // 
+            // labelNazwisko
+            // 
+            this.labelNazwisko.AutoSize = true;
+            this.labelNazwisko.Location = new System.Drawing.Point(37, 60);
+            this.labelNazwisko.Name = "labelNazwisko";
+            this.labelNazwisko.Size = new System.Drawing.Size(53, 13);
+            this.labelNazwisko.TabIndex = 1;
+            this.labelNazwisko.Text = "Nazwisko";
+            // 
+            // labelWiek
+            // 
+            this.labelWiek.AutoSize = true;
+            this.labelWiek.Location = new System.Drawing.Point(37, 88);
+            this.labelWiek.Name = "labelWiek";
+            this.labelWiek.Size = new System.Drawing.Size(32, 13);
+            this.labelWiek.TabIndex = 2;
+            this.labelWiek.Text = "Wiek";
+            this.labelWiek.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxImie
+            // 
+            this.textBoxImie.Location = new System.Drawing.Point(93, 31);
+            this.textBoxImie.Name = "textBoxImie";
+            this.textBoxImie.Size = new System.Drawing.Size(100, 20);
+            this.textBoxImie.TabIndex = 3;
+            // 
+            // textBoxNazwisko
+            // 
+            this.textBoxNazwisko.Location = new System.Drawing.Point(93, 60);
+            this.textBoxNazwisko.Name = "textBoxNazwisko";
+            this.textBoxNazwisko.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNazwisko.TabIndex = 4;
+            // 
+            // textBoxWiek
+            // 
+            this.textBoxWiek.Location = new System.Drawing.Point(93, 88);
+            this.textBoxWiek.Name = "textBoxWiek";
+            this.textBoxWiek.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWiek.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelKomunikat1
+            // 
+            this.labelKomunikat1.AutoSize = true;
+            this.labelKomunikat1.Location = new System.Drawing.Point(40, 161);
+            this.labelKomunikat1.Name = "labelKomunikat1";
+            this.labelKomunikat1.Size = new System.Drawing.Size(0, 13);
+            this.labelKomunikat1.TabIndex = 7;
+            // 
+            // labelKomunikat2
+            // 
+            this.labelKomunikat2.AutoSize = true;
+            this.labelKomunikat2.Location = new System.Drawing.Point(40, 190);
+            this.labelKomunikat2.Name = "labelKomunikat2";
+            this.labelKomunikat2.Size = new System.Drawing.Size(0, 13);
+            this.labelKomunikat2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 450);
+            this.Controls.Add(this.labelKomunikat2);
+            this.Controls.Add(this.labelKomunikat1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxWiek);
+            this.Controls.Add(this.textBoxNazwisko);
+            this.Controls.Add(this.textBoxImie);
+            this.Controls.Add(this.labelWiek);
+            this.Controls.Add(this.labelNazwisko);
+            this.Controls.Add(this.labelImie);
+            this.Name = "Form1";
+            this.Text = "Aplikacja witaj";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelImie;
+        private System.Windows.Forms.Label labelNazwisko;
+        private System.Windows.Forms.Label labelWiek;
+        private System.Windows.Forms.MaskedTextBox textBoxImie;
+        private System.Windows.Forms.MaskedTextBox textBoxNazwisko;
+        private System.Windows.Forms.MaskedTextBox textBoxWiek;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelKomunikat1;
+        private System.Windows.Forms.Label labelKomunikat2;
+    }
+}
+
---
+++ GraZaDuzoZaMalo/HelloWinFormsApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labelKomunikat1.Text = "Hej, " + textBoxImie.Text + " " + textBoxNazwisko.Text;
+
+            int wiek = Convert.ToInt32(textBoxWiek.Text);
+            if (wiek > 0 && wiek < 67)
+            {
+                labelKomunikat2.Text=string.Format("do emerytury zostalo ci {0} lat", 67 - wiek);
+            }
+            else if (wiek < 0)
+            {
+                labelKomunikat2.Text = "Podano błędne dane!";
+
+            }
+            else
+            {
+                labelKomunikat2.Text = "Emeryt :)";
+            }
+
+        }
+         
+    }
+}
